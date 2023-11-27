d43300a15e5f1d766edb8842048fff3986a0e020
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-27 16:52:06 -0500

    added the ability to upload spss for predictions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# main.py
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
+import pandas as pd
+import os
+import shutil
 from model import BartModel, process_input_data, generate_sample_data
 from pydantic import BaseModel
 from typing import List
@@ -8,22 +12,26 @@ import numpy as np
 app = FastAPI()
 model = BartModel()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello TxBSPI!"}
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Define a Pydantic model for the input data
 class ModelInput(BaseModel):
     X: List[List[float]]  # Assuming X is a 2D array (list of lists)
     y: List[float]        # Assuming y is a 1D array (list)
 
+@app.get("/")
+async def main():
+    return FileResponse('static/index.html') # Serve index.html from /static
+
 @app.post("/predict")
 async def make_prediction(input_data: ModelInput):
+    # print(f"Received data: X - {input_data.X}, y - {input_data.y}")
     try:
         X = np.array(input_data.X)
         y = np.array(input_data.y)
-        print(f"Data Shape: X - {X.shape}, y - {y.shape}") # Log data shape
-        print(f"Sample Data: X - {X[:5]}, y - {y[:5]}") # Log sample data
+        # print(f"Data Shape: X - {X.shape}, y - {y.shape}") # Log data shape
+        # print(f"Sample Data: X - {X[:5]}, y - {y[:5]}") # Log sample data
         model.fit(X, y)
         predictions = model.predict(X)
         return {"predictions": predictions.tolist()}
@@ -42,3 +50,20 @@ async def test_prediction():
     except Exception as e:
         print(f"Error during test prediction: {e}")
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    if not file.filename.endswith('.spss'):
+        raise HTTPException(status_code=400, detail="Invalid file type")
+
+    temp_file_path = f'temp_{file.filename}'
+    with open(temp_file_path, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    try:
+        df = pd.read_spss(temp_file_path)
+        # Return the entire DataFrame
+        return {"uploadedData": df.to_dict(orient='records')}
+    except Exception as e:
+        os.remove(temp_file_path)
+        raise HTTPException(status_code=500, detail=f"Error reading SPSS file: {e}")
