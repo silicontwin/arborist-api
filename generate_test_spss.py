50c0459221a59a4bb1724ef37ef06fc5c7e1e3de
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-27 17:10:21 -0500

    revised test data generator to match example

--- generate_test_spss.py
+++ generate_test_spss.py
@@ -1,16 +1,24 @@
 # generate_test_spss.py
+import numpy as np
 import pandas as pd
 import pyreadstat  # Required for saving to SPSS format
 
-# Generate sample data
-n_samples = 1000
-data = {
-    'feature1': pd.Series(range(n_samples)),
-    'feature2': pd.Series(range(n_samples)),
-    'y': pd.Series(range(n_samples))
-}
+# Set random number generator
+rng = np.random.default_rng(101)
+
+# Define sample size and number of features
+n = 1000
+p = 10
+
+# Generate random uniform features
+X = rng.uniform(0, 1, (n, p))
+
+# Generate target variable 'y' based on a function of the features
+y = X[:, 0] * 100 + X[:, 1] * 2 + rng.normal(0, 1, n)
 
 # Create DataFrame
+data = {'feature' + str(i+1): X[:, i] for i in range(p)}
+data['y'] = y
 df = pd.DataFrame(data)
 
 # Save DataFrame as SPSS file
