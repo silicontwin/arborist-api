388a998ec3afde3a665c0837490e81d788c89fee
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-05 16:42:53 -0500

    added endpoints and functionality from app tests

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import os
 import shutil
@@ -8,14 +9,26 @@ from model import BartModel, process_input_data, generate_sample_data
 from pydantic import BaseModel
 from typing import List
 import numpy as np
+import errno
+import uvicorn
 
 # ------------------------------------------------------------------------------
 
 app = FastAPI()
 model = BartModel()
 
-# Mount static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# # Mount static files
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 # ------------------------------------------------------------------------------
 
@@ -26,9 +39,15 @@ class ModelInput(BaseModel):
 
 # ------------------------------------------------------------------------------
 
-@app.get("/")
-async def main():
-    return FileResponse('static/index.html') # Serve index.html from /static
+# @app.get("/")
+# async def main():
+#     return FileResponse('static/index.html') # Serve index.html from /static
+
+# ------------------------------------------------------------------------------
+
+@app.get("/data")
+def read_data():
+    return {"status": "active"}
 
 # ------------------------------------------------------------------------------
 
@@ -79,3 +98,18 @@ async def create_upload_file(file: UploadFile = File(...)):
     except Exception as e:
         os.remove(temp_file_path)
         raise HTTPException(status_code=500, detail=f"Error reading SPSS file: {e}")
+
+# ------------------------------------------------------------------------------
+
+# Run server with uvicorn (and check for port collisions)
+def run_server(port=8000):
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=port)
+    except OSError as e:
+        if e.errno == errno.EADDRINUSE:
+            print(f"Port {port} is in use. Server already running.")
+        else:
+            raise
+
+if __name__ == "__main__":
+    run_server()
