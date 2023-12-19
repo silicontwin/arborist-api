cc2530992521700eac7dc7256302a2592d54d6b6
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-19 10:18:42 -0500

    refactored to save test data in the same directory

--- helpers/generate_test_spss.py
+++ helpers/generate_test_spss.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import pyreadstat  # Required for saving to SPSS format
+import os
 
 # Set random number generator
 rng = np.random.default_rng(101)
@@ -21,8 +22,16 @@ data = {'feature' + str(i+1): X[:, i] for i in range(p)}
 data['y'] = y
 df = pd.DataFrame(data)
 
-# Save DataFrame as SPSS file
+# Define the filename for the SPSS file
 spss_filename = 'test_data.spss'
-pyreadstat.write_sav(df, spss_filename)
 
-print(f"SPSS file '{spss_filename}' has been created.")
+# Get the current directory of the script
+current_dir = os.path.dirname(__file__)
+
+# Define the file path for saving the SPSS file in the current directory
+spss_file_path = os.path.join(current_dir, spss_filename)
+
+# Save the DataFrame to SPSS in the current directory
+pyreadstat.write_sav(df, spss_file_path)
+
+print(f"SPSS file '{spss_file_path}' has been created.")
