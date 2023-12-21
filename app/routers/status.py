0701b0278907c8e7e61f46bbb89a0a156738d299
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-21 12:15:03 -0500

    added status endpoint documentation

--- app/routers/status.py
+++ app/routers/status.py
@@ -1,7 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+from typing import Dict
 
 router = APIRouter()
 
-@router.get("/status")
+@router.get(
+    "/status", 
+    summary="Check that the API is online [Arborist App]",
+    description="This endpoint checks if the API is online and reachable.",
+    response_description="The status of the API",
+    response_model=Dict[str, str],
+    responses={
+        200: {"description": "API is online"},
+        500: {"description": "Internal server error"}
+    }
+)
 def read_data():
+    """
+    Read the status of the API.
+
+    This can be used to check the health or availability of the API.
+    Returns a JSON object with the current status.
+    """
     return {"api": "online"}
