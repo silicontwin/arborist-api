909ef55f9fc8de0e1fdeaeabf8170992d181f4af
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-01-04 09:51:43 -0500

    added temp settings endpoint

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import RedirectResponse
 from app.routers.predict import router as predict
 from app.routers.status import router as status
 from app.routers.upload import router as upload
+from app.routers.settings import router as settings
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -24,6 +25,7 @@ app.add_middleware(
 app.include_router(status)
 app.include_router(predict)
 app.include_router(upload)
+app.include_router(settings)
 
 @app.get("/")
 async def root():
